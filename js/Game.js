597b662d9df6435d6aa085ac183d2a73168f7b76
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-08-11 20:35:42 +0200

    all code well

--- js/Game.js
+++ js/Game.js
@@ -3,6 +3,8 @@ import { UI } from "./UI.js";
 import { Counter } from "./Counter.js";
 import { Timer } from "./Timer.js";
 import { ResetButton } from "./ResetButton.js";
+import { Modal } from "./Modal.js";
+
 class Game extends UI {
   #config = {
     easy: {
@@ -24,6 +26,7 @@ class Game extends UI {
 
   #counter = new Counter();
   #timer = new Timer();
+  #modal = new Modal();
 
   #isGameFinished = false;
   #numberOfRows = null;
@@ -73,15 +76,35 @@ class Game extends UI {
 
     this.#cellsElements = this.getElements(this.UiSelectors.cell);
 
+    this.#buttons.reset.changeEmotion("neutral");
+
+    this.#isGameFinished = false;
+    this.#revealedCells = 0;
+
     this.#addCellsEventListeners();
   }
   #endGame(isWin) {
     this.#isGameFinished = true;
     this.#timer.stopTimer();
+    this.#modal.buttonText = "Close";
 
     if (!isWin) {
       this.#revealMines();
+      this.#modal.infoText = "You lost, try again";
+      this.#buttons.reset.changeEmotion("negative");
+      this.#modal.setText();
+      this.#modal.toggleModal();
+      return;
     }
+    this.#modal.infoText =
+      this.#timer.numberOfSeconds < this.#timer.maxNumberOfSeconds
+        ? `You won! It took you ${
+            this.#timer.numberOfSeconds
+          } second, congratulations`
+        : "You won, congratulation";
+    this.#buttons.reset.changeEmotion("positive");
+    this.#modal.setText();
+    this.#modal.toggleModal();
   }
 
   #handleElements() {
@@ -107,6 +130,7 @@ class Game extends UI {
   }
 
   #addButtonsEventListeners() {
+    this.#buttons.modal.addEventListener("click", this.#modal.toggleModal);
     this.#buttons.easy.addEventListener("click", () => {
       this.#handleNewGameClick(
         this.#config.easy.rows,
